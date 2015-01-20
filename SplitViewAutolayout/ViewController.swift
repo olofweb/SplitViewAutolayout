efceea414a9dd6c80df772458c3be20804851d2f
Olof <olof@sunrise.ch>
2015-01-20 18:25:42 +0100

    Initial Commit

---
+++ SplitViewAutolayout/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SplitViewAutolayout
+//
+//  Created by Olof on 20.01.15.
+//  Copyright (c) 2015 Olosoft. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
